83fc44f52308d34c4788b7e3ab25ab4574a60e90
John Njuki <njukijohn2000@gmail.com>
2023-08-30 20:08:13 +0300

    add the products entity

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import {CldUploadWidget} from "next-cloudinary"
+import { CldUploadWidget } from "next-cloudinary";
 
 import { Button } from "./button";
 import { ImagePlus, Trash } from "lucide-react";
@@ -44,6 +44,7 @@ export const ImageUpload: React.FC<ImageUploadProps> = ({
           >
             <div className="z-10 absolute top-2 right-2">
               <Button
+                disabled={disabled}
                 type="button"
                 onClick={() => onRemove(url)}
                 variant="destructive"
@@ -57,17 +58,22 @@ export const ImageUpload: React.FC<ImageUploadProps> = ({
         ))}
       </div>
       <CldUploadWidget onUpload={onUpload} uploadPreset="ckdkddbg">
-        {({open}) => {
-          const onClick =() => {
+        {({ open }) => {
+          const onClick = () => {
             open();
-          }
+          };
 
           return (
-            <Button type="button" disabled={disabled} variant="secondary" onClick={onClick}>
+            <Button
+              type="button"
+              disabled={disabled}
+              variant="secondary"
+              onClick={onClick}
+            >
               <ImagePlus className="h-4 w-4 mr-2" />
               Upload an Image
             </Button>
-          )
+          );
         }}
       </CldUploadWidget>
     </div>
